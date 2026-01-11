e94a59f061cbb39be3e15443cc9c284db9052528
orion <168399187+fandombreh@users.noreply.github.com>
2026-01-11 13:24:16 -0700

    Implement singleton pattern in AntiCheatHandlerBase

--- EIOP/Anti Cheat/AntiCheatHandlerBase.cs
+++ EIOP/Anti Cheat/AntiCheatHandlerBase.cs
@@ -2,7 +2,39 @@ using UnityEngine;
 
 namespace EIOP.Anti_Cheat;
 
+[DisallowMultipleComponent]
 public class AntiCheatHandlerBase : MonoBehaviour
 {
-    // empty for now, i just dont fw manually adding components
+    private static AntiCheatHandlerBase instance;
+
+    public static AntiCheatHandlerBase Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<AntiCheatHandlerBase>();
+                
+                if (instance == null)
+                {
+                    var go = new GameObject("AntiCheatHandler");
+                    instance = go.AddComponent<AntiCheatHandlerBase>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 }
