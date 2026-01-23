27958bc2efdaa5b45f00de038aca57b9377f601f
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-01-23 17:43:27 +0100

    small but needed console changes

--- EIOP/Constants.cs
+++ EIOP/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string PluginGuid    = "hansolo1000falcon.eiop";
     public const string PluginName    = "EIOP";
-    public const string PluginVersion = "1.1.0";
+    public const string PluginVersion = "1.1.1";
 }
